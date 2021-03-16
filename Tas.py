11e5bfcf8bf7edf6756e823d58777f36ac5a36f7
Naser Gholami <80551568+Na3r94@users.noreply.github.com>
2021-03-16 14:51:37 +0330

    Update Tas.py

--- Tas.py
+++ Tas.py
@@ -4,8 +4,11 @@ import random
 Num1 = random.randint( 1 , 6)
 print(Num1)
 
+Num2 = 6
+
 if Num1 == 6:
-    print("Jaize darid,Dobare bendazid")
-    Num2 = random.randint(1 ,6)
-    print(Num2)
+    while Num2 == 6:
+        print("Jaize darid,Dobare bendazid")
+        Num2 = random.randint(1 ,6)
+        print(Num2)
 
